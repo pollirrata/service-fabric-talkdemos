1f283621fc90386fa033afba07aedae3e0e2915e
Jorge Gaona <jorgegaona@yahoo.com>
2017-02-28 00:47:02 -0600

    webapi services

---
+++ spacespiders/TraditionalBackend/SpaceSpiders.Services/SpaceSpiders.Services/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpaceSpiders.Services.Startup))]
+namespace SpaceSpiders.Services
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {        }
+    }
+}
